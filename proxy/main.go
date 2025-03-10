77b7a0cde222ed13b92808ff34192cf5196c70f7
Zhodaran <120091328+Zhodaran@users.noreply.github.com>
2025-03-10 15:33:40 +0300

    Update main.go

--- proxy/main.go
+++ proxy/main.go
@@ -579,6 +579,8 @@ func addBookHandler(resp controller.Responder, db *sql.DB) http.HandlerFunc {
 // @Router /api/users [get]
 func listUsersHandler(resp controller.Responder) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		  mu.Lock() 
+        defer mu.Unlock() 
 		// Здесь предполагается, что Users - это глобальная переменная, содержащая всех пользователей
 		var users []auth.User
 		for _, user := range auth.Users {
